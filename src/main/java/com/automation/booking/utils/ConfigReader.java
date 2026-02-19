52a03f59fef8b55e9f3f8a0458f8fc39019b540e
Giorgi Kharebava <kharebavagiorgi01@gmail.com>
2026-02-19 17:56:06 +0400

    feat: implement centralized configuration management with ConfigReader

---
+++ src/main/java/com/automation/booking/utils/ConfigReader.java
@@ -0,0 +1,26 @@
+package com.automation.booking.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try(InputStream inputStream = ConfigReader.class.getClassLoader().getResourceAsStream(
+                "config.properties")){
+            if(inputStream == null){
+                throw new RuntimeException("Could not find config.properties!");
+            }
+            properties.load(inputStream);
+        }catch (IOException e){
+            throw new RuntimeException("Could not properties file");
+        }
+    }
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
